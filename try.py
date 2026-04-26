5650e8a4453b593eb4056187c8103c67630a0324
melaEM <mihaelaem1234@gmail.com>
2026-04-26 10:44:14 +0300

    Randomizing reservations attempt, timestamps dont work :(

--- try.py
+++ try.py
@@ -1,38 +1,41 @@
-# import random
+import random
+import datetime
+from datetime import timedelta
+import time
+days_selected = True
 
-# class Reservations:
-#     def __init__(self, room_type, start_date, end_date, alr_reserved_on):
-#         self.room_type = room_type
-#         self.start_date = start_date
-#         self.end_date = end_date
-#         self.alr_reserved_on = alr_reserved_on
+class Reservations:
+    def __init__(self, room_type, start_date, end_date, alr_reserved_on):
+        self.room_type = room_type
+        self.start_date = start_date
+        self.end_date = end_date
+        self.alr_reserved_on = alr_reserved_on
 
-#         def reserve_dates(start_date, end_date):
-            
+    def reserve_dates(start_date, end_date):
+        pass
 
-# room_count = 101
-# room_list = []
-# import random
+arrival_date = input("arrival")
+departure_date = input("departure")    
 
-# if:
-#     hhgg
-#     for:
-#         fdef
-#     else:
-#         df
+room_count = 101
+room_list = []
 
+Alr_reserved = False
+min_date_evil = datetime.date.today() + datetime.timedelta(days=1)
+max_date_evil = datetime.date(2027, 12, 31)
+allowed_range = (max_date_evil - min_date_evil).days
 
-# for i in range(random.randint(3, 9)):
-#     print("ff")
-#     room_list.append(room_count)
-#     rand_type = random.choice(["Single","Double"])
-#     for x in range
-#     begin_rand_reserv = random.choice
-#     room_list[i] = Reservations(rand_type, )
-#     print(room_list[i].room_type)
-#     print(room_list[i].reservations)
-#     print(room_list)
-
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-get_letter = alphabet.index('a')
-print(alphabet(get_letter))
+for random_reservation_amount in range(random.randint(3, 8)):
+    room_list.append(room_count)
+    rand_type = random.choice(["Single","Double"])
+    rand_start = random.randint(0, allowed_range)
+    rand_end = random.randint(1,14)
+    rand_arr_date = min_date_evil + datetime.timedelta(days=rand_start)
+    rand_dep_date = rand_arr_date + datetime.timedelta(days=rand_end)
+    arr_timestamp = timestamp(rand_arr_date)
+    dep_timestamp = timestamp(rand_dep_date)
+    if days_selected and arr_timestamp <timestamp(arrival_date)> dep_timestamp or arr_timestamp <timestamp(departure_date)> dep_timestamp:
+        Alr_reserved = True 
+    room_list[i] = Reservations(rand_type,arr_timestamp,dep_timestamp,Alr_reserved)
+    print(room_list[i].room_type)
+    print(room_list[i].reservations)
