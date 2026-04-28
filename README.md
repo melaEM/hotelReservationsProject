ebd7fb1b768ee9a5ba8eee2835a23d5a3d9a6d74
melaEM <mihaelaem1234@gmail.com>
2026-04-28 18:15:03 +0300

    Finalized try py pre-merge with hotel py

--- README.md
+++ README.md
@@ -5,4 +5,9 @@ A school project using python to create a basic hotel reservation system.
 It takes a start and end date and then shows the rooms available at the time that also match the room type selected.
 
 ## Hotel.py
-It consists of a tkinter window that has a button which opens a Toplevel window with Dateentrys that allow for the date selection as well as a optionsmenu that allows for selection of the room type. When an option in the Optionsmenu isn't selected rooms with either type will be shown, but if dates aren't selected reservation will now be allowed.
+It consists of a tkinter window that has a button which opens a Toplevel window with Dateentrys that allow for the date selection as well as a optionsmenu that allows for selection of the room type. When an option in the Optionsmenu isn't selected rooms with either type will be shown, but if dates aren't selected reservation will not be allowed.
+
+
+---------
+### ink.png 
+A note from google keep used to see how the timestamp comparison should work (added since I will 100% won't be able to remember the logic by tmrw)
--- try.py
+++ try.py
@@ -1,9 +1,6 @@
 import random
-import datetime
-from datetime import timedelta, datetime
+from datetime import timedelta, datetime, date
 import time
-from datetime import date
-from tkinter.ttk import *
 from tkinter import *
 from tkcalendar import DateEntry
 days_selected = True
@@ -29,9 +26,9 @@ max_date_evil = date(2027, 12, 31)
 
 allowed_range = (max_date_evil - min_date_evil).days
 
-arrival_date = date(2026, 10,16)
+arrival_date = date(2027, 2,1)
 arrival_date = arrival_date.strftime("%d/%m/%Y")
-departure_date = date(2026, 10, 19)
+departure_date = date(2027, 5, 20)
 departure_date = departure_date.strftime("%d/%m/%Y")
 
 for random_reservation_amount in range(random.randint(3, 8)):
@@ -52,8 +49,21 @@ for random_reservation_amount in range(random.randint(3, 8)):
     if days_selected:
         user_arrival_stamp = date.strptime(str(arrival_date), "%d/%m/%Y")
         user_departure_stamp = date.strptime(str(departure_date), "%d/%m/%Y")
-        if arr_timestamp < user_arrival_stamp < dep_timestamp or arr_timestamp < user_departure_stamp < dep_timestamp:
+        # print(f"arr_date < dep_stamp: {user_arrival_stamp < dep_timestamp} \, arr_date > arr_stamp: {arr_timestamp < user_arrival_stamp}")
+        # print(f"dep_date < dep_stamp: {user_arrival_stamp < dep_timestamp} \, dep_date > arr_stamp: {arr_timestamp < user_arrival_stamp}")
+        # if arr_timestamp <= user_arrival_stamp <= dep_timestamp or arr_timestamp >= user_departure_stamp >= dep_timestamp:
+        stamp_check_1 = user_arrival_stamp <= arr_timestamp < dep_timestamp <= user_departure_stamp
+        stamp_check_2 = arr_timestamp <= user_arrival_stamp < user_departure_stamp <= dep_timestamp
+        stamp_check_3 = user_arrival_stamp <= arr_timestamp < user_departure_stamp <= dep_timestamp
+        stamp_check_4 = arr_timestamp <= user_arrival_stamp < dep_timestamp <= user_departure_stamp
+        if stamp_check_1 or stamp_check_2 or stamp_check_3 or stamp_check_4:
             Alr_reserved = True 
+            # print(stamp_check_1)
+            # print(stamp_check_2)
+            # print(stamp_check_3)
+            # print(stamp_check_4)
+        else:
+            Alr_reserved = False
     room_list[random_reservation_amount] = Reservations(rand_type,arr_timestamp,dep_timestamp,Alr_reserved)
     print(room_list[random_reservation_amount].room_type)
     print(room_list[random_reservation_amount].alr_reserved_on)
